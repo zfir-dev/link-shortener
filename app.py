78d4ac747ffb7f9196cee0b4e4268fd47428426d
Zafir Sk Heerah <zafir.heerah@spoonconsulting.com>
2025-04-24 23:13:56 +0400

    refactor

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def two_factor_required(func):
         otp_generated_at = session.get("otp_generated_at")
         current_time = datetime.now().timestamp()
         
-        if not otp_generated_at or (current_time - float(otp_generated_at)) > 10:
+        if not otp_generated_at or (current_time - float(otp_generated_at)) > 3600:
             logout_user()
             session.pop("two_factor_authenticated", None)
             session.pop("otp_generated_at", None)
@@ -279,19 +279,9 @@ def two_factor():
         code = request.form.get("code")
         totp = pyotp.TOTP(current_user.totp_secret)
         
-        otp_generated_at = session.get("otp_generated_at")
-        current_time = datetime.now().timestamp()
-        
-        if not otp_generated_at or (current_time - float(otp_generated_at)) > 3600:
-            flash("2FA code has expired. Please log in again.", "error")
-            logout_user()
-            session.pop("two_factor_authenticated", None)
-            session.pop("otp_generated_at", None)
-            return redirect(url_for("login"))
-            
         if totp.verify(code):
             session["two_factor_authenticated"] = True
-            session["otp_generated_at"] = current_time
+            session["otp_generated_at"] = datetime.now().timestamp()
             flash("2FA successful. You are now logged in.", "success")
             next_page = request.args.get("next") or url_for("index")
             return redirect(next_page)
